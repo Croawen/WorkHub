4b33a1b8753d25bb75f7cbfc299803b1f6134c25
Croawen <croawen@gmail.com>
2016-11-06 16:17:08 +0100

    Initial

---
+++ WorkHub/WorkHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WorkHub.Startup))]
+namespace WorkHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
