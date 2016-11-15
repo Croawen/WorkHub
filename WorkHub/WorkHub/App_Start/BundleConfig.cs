ae402a9a5bed9dc7314c5829e3b2a6807faf2094
Konrad <konek3@hotmail.com>
2016-11-15 18:42:18 +0100

    Redo Index landing page and styles, images

--- WorkHub/WorkHub/App_Start/BundleConfig.cs
+++ WorkHub/WorkHub/App_Start/BundleConfig.cs
@@ -26,6 +26,8 @@ namespace WorkHub
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/Index/css").Include(
+                "~/Content/Index/index-style.css"));
         }
     }
 }
